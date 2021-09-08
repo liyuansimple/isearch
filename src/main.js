291582b20aada31f78d96b45e41c2a89fcce1b4f
liyuansimple <liyuan@ecdpower.net>
2021-09-08 14:59:33 +0800

    自己定制全局组件

--- src/main.js
+++ src/main.js
@@ -1,7 +1,7 @@
 /*
  * @Author: your name
  * @Date: 2021-07-22 16:30:23
- * @LastEditTime: 2021-07-27 13:49:25
+ * @LastEditTime: 2021-09-08 14:46:59
  * @LastEditors: Please set LastEditors
  * @Description: In User Settings Edit
  * @FilePath: \SCRM-Admin-Webd:\work\project\isearch\src\main.js
@@ -14,10 +14,11 @@ import store from './store'
 import Element from 'element-ui'
 import 'element-ui/lib/theme-chalk/index.css';
 import '@/styles/index.scss'
-
+import alert from '@/components/packages/alert'
 Vue.config.productionTip = false
 
 Vue.use(Element)
+Vue.use(alert)
 
 new Vue({
   router,
